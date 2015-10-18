4961e1e4fd6f2b85fa3760e85c6244b3fcd39131
Chava Jurado <chava.jurado@gmail.com>
2015-10-18 18:32:03 +0000

    worked a bit more on the default_handler

--- oldpeculier/rest/server.py
+++ oldpeculier/rest/server.py
@@ -4,6 +4,7 @@ import re
 import socket
 import signal
 import sys
+import urlparse
 
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from multiprocessing import Process, Event, Semaphore, Value, cpu_count
@@ -82,7 +83,22 @@ def default_handler(request):
     message += "REQUEST HEADERS\n"
     for header in request.headers.keys():
         message += " > {0}: {1}\n".format(header,request.headers.get(header))
-        
+    message += "\nREQUEST PARAMETERS\n" 
+    parsed_url = urlparse.urlparse(request.path)
+    if  parsed_url.query:
+        params = urlparse.parse_qs(parsed_url.query)
+        for param in params.keys():
+            value=params.get(param)
+            if isinstance(value,list):
+                for v in value:
+                    message += " > {0}: {1}\n".format(param,v)
+            else:
+                message += " > {0}: {1}\n".format(param,params.get(param))
+    message += "\nREQUEST BODY\n" 
+    if request.headers.has_key('Content-Length'):
+        contentlength = int(request.headers.get('Content-Length'))
+        body = request.rfile.read(contentlength)
+        message += body+"\n"
     request.send_response(200)
     request.send_header("Content-Length",len(message))
     request.end_headers()
@@ -108,6 +124,20 @@ class RestServer(PooledProcessMixIn, HTTPServer, Common):
 
 class RestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        match_length=0
+        route_handler=None
+        for route in self.server.routes["GET"]:
+            for url in route["urls"]:
+                match = re.compile("("+url+")").match(self.path)
+                if hasattr(match,"groups"):
+                    matched_group = match.group()
+                    if match_length < matched_group:
+                        match_length= len(matched_group)
+                        route_handler = route["handler"]
+        if route_handler:
+            route_handler(self)
+
+    def do_POST(self):
         match_length=0
         route_handler=None
         for route in self.server.routes["GET"]:
